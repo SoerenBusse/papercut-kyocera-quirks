1825c5920812458f35d9a2fb5a4ffcfd20169c9c
Waishon <mail@sbusse.de>
2024-10-15 18:47:45 +0200

    - Fix typo in installation

--- install.sh
+++ install.sh
@@ -4,7 +4,7 @@ cp cups/filter/pdftoquirks.py /usr/lib/cups/filter/pdftoquirks
 chmod +x /usr/lib/cups/filter/pdftoquirks
 
 echo "Copy Backend"
-cp cups/backend/quirkstoipp.py /usr/lib/cups/quirkstoipp
-chmod +x /usr/lib/cups/quirkstoipp
+cp cups/backend/quirkstoipp.py /usr/lib/cups/backend/quirkstoipp
+chmod +x /usr/lib/cups/backend/quirkstoipp
 
 systemctl restart cups
